8ad427456aab021a3e955ecf3210337574f49025
kary_ser <morskya.life@gmail.com>
2023-09-18 16:08:05 +0300

    added list menu

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,9 +1,12 @@
 const hamEl = document.querySelector('.hamburger')
+ const mobileEl = document.querySelector('.mobile-menu')
  hamEl.addEventListener('click', () => {
 hamEl.classList.toggle('ham-active')
+mobileEl.classList.toggle('active-box')
  })
 
  const menuEl = document.querySelectorAll('.header__menu')
+
  window.addEventListener('resize', function () {
   // if (window.innerWidth <= 968) {
   //   hamEl.classList.add('ham-mobile')
@@ -11,5 +14,7 @@ hamEl.classList.toggle('ham-active')
   //   hamEl.classList.remove('ham-mobile')
   // }
 
-  window.innerWidth <= 968 ?  hamEl.classList.add('ham-mobile'): hamEl.classList.remove('ham-mobile')
+  window.innerWidth <= 968 ?  hamEl.classList.add('ham-mobile') 
+  : hamEl.classList.remove('ham-mobile')
 })
+
