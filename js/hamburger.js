db2446db5c94a7a10b3e006ba85bb12a8322a8d1
Kary <morskya.life@gmail.com>
2023-09-19 13:06:19 +0300

    added setTimeout for hamburger

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,20 +1,23 @@
 const hamEl = document.querySelector('.hamburger')
- const mobileEl = document.querySelector('.mobile-menu')
+const mobileEl = document.querySelector('.mobile-menu')
+
  hamEl.addEventListener('click', () => {
-hamEl.classList.toggle('ham-active')
-mobileEl.classList.toggle('active-box')
+ hamEl.classList.toggle('ham-active')
+ mobileEl.classList.toggle('active-box')
  })
 
- const menuEl = document.querySelectorAll('.header__menu')
+const menuEl = document.querySelectorAll('.header__menu')
 
  window.addEventListener('resize', function () {
-  // if (window.innerWidth <= 968) {
-  //   hamEl.classList.add('ham-mobile')
-  // } else {
-  //   hamEl.classList.remove('ham-mobile')
-  // }
-
-  window.innerWidth <= 968 ?  hamEl.classList.add('ham-mobile') 
-  : hamEl.classList.remove('ham-mobile')
+ window.innerWidth <= 968 ?  
+  hamEl.classList.add('ham-mobile') : hamEl.classList.remove('ham-mobile')
 })
+
+document.querySelectorAll('.header__menu-link').forEach((el) => el.addEventListener('click', () => {
+  setTimeout(() => {
+    mobileEl.classList.remove('active-box')
+    hamEl.classList.remove('ham-active')
+  }, 1000)
+ })
+ )
 
