186abe7ce611734d6940a295bfd5bfc28fdf5ac2
Kary <morskya.life@gmail.com>
2023-09-19 15:27:03 +0300

    added 3d animation

--- js/fire.js
+++ js/fire.js
@@ -1,4 +1,4 @@
-const fireContainer = document.getElementById("fire-container")
+const fireContainer = document.getElementById('fire-container')
 
 function createParticles(fireContainer, num, leftSpacing) {
   for (let i = 0; i < num; i += 1) {
@@ -11,3 +11,18 @@ function createParticles(fireContainer, num, leftSpacing) {
 }
 
 createParticles(fireContainer, 70, 40)
+
+
+const fireBtnContainer = document.getElementById('fire2-container')
+
+function createBtnParticles(fireBtnContainer, num, leftSpacing) {
+  for (let j = 0; j < num; j += 1) {
+    let particleEl = document.createElement("div")
+    particleEl.style.left = `calc((100% + 15em) * ${j / leftSpacing })`
+    particleEl.setAttribute("class", "particleEl")
+    particleEl.style.animationDelay = 8 * Math.random() + "s"
+    fireBtnContainer.appendChild(particleEl)
+  }
+}
+
+createBtnParticles(fireBtnContainer, 20, 30)
